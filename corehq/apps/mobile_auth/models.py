d2904d3e9c1dab209283f5cd58762d7fc88f1393
Jennifer Schweers <orange.jenny@gmail.com>
2020-05-08 17:22:25 -0400

    Added SQLMobileAuthKeyRecord (PR1)

--- corehq/apps/mobile_auth/models.py
+++ corehq/apps/mobile_auth/models.py
@@ -1,5 +1,7 @@
 import uuid
 
+from django.db import models
+
 from dimagi.ext.couchdbkit import DateTimeProperty, Document, StringProperty
 from dimagi.utils.parsing import json_format_datetime
 
@@ -29,6 +31,7 @@ class MobileAuthKeyRecord(Document):
         if not self._id:
             self._id = uuid.uuid4().hex
 
+    # TODO: PR1
     @property
     def uuid(self):
         return self.get_id
@@ -44,3 +47,18 @@ class MobileAuthKeyRecord(Document):
             include_docs=True,
         ).first()
         return key_record
+
+
+class SQLMobileAuthKeyRecord(models.Model):
+    domain = models.CharField(max_length=126, null=False, db_index=True)
+    user_id = models.CharField(max_length=255, null=False, db_index=True)
+
+    valid = models.DateTimeField(null=False)    # initialized with 30 days before the date created
+    expires = models.DateTimeField(null=False)  # just bumped up by multiple of 30 days when expired
+    type = models.CharField(null=False, max_length=32, choices=[('AES256', 'AES256')], default='AES256')
+    key = models.CharField(null=False, max_length=127)
+
+    couch_id = models.CharField(max_length=126, null=True, db_index=True)
+
+    class Meta:
+        db_table = "mobile_auth_mobileauthkeyrecord"
