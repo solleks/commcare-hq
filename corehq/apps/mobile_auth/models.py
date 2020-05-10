dae7f37af54c5b7d8ecb365f73b79aa3a86dda3a
Jennifer Schweers <orange.jenny@gmail.com>
2020-05-10 17:42:17 -0400

    Renamed sql model (PR3)

--- corehq/apps/mobile_auth/models.py
+++ corehq/apps/mobile_auth/models.py
@@ -8,7 +8,7 @@ from dimagi.utils.parsing import json_format_datetime
 from .utils import generate_aes_key
 
 
-class SQLMobileAuthKeyRecord(models.Model):
+class MobileAuthKeyRecord(models.Model):
     domain = models.CharField(max_length=126, null=False, db_index=True)
     user_id = models.CharField(max_length=255, null=False, db_index=True)
 
@@ -16,6 +16,3 @@ class SQLMobileAuthKeyRecord(models.Model):
     expires = models.DateTimeField(null=False)  # just bumped up by multiple of 30 days when expired
     type = models.CharField(null=False, max_length=32, choices=[('AES256', 'AES256')], default='AES256')
     key = models.CharField(null=False, max_length=127)
-
-    class Meta:
-        db_table = "mobile_auth_mobileauthkeyrecord"
